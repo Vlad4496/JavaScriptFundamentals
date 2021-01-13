6ac646638140ee8178b1ad18c17a96a6e2c0f0d0
Vlad4496 <vlad_4496@yahoo.com>
2021-01-13 16:28:24 +0200

    TemaInmultire

--- inmultire/inmultire.js
+++ inmultire/inmultire.js
@@ -1,9 +1,12 @@
-// let j = 3
-
-for (let j = 0; j < 10; j++) {
-    for (let i = 0; i < 10; i++) {
-        let produs = i * j
-        document.write(i + " x " + j + " = " + produs + "<br>")
+document.write("<section>")
+for(i=0; i<15; i++) {
+    document.write("<div>")
+    for(j=0; j<15; j++) {
+        let bucle = i * j
+        document.write(i + "x" + j + "=" + bucle + "<br>")
     }
     document.write("<br>")
+    document.write("</div>")
 }
+
+document.write("</section>")
